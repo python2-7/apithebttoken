c26a06eb2a76f0076521dc013c946c19dd082499
Your Name <you@example.com>
2019-06-21 08:55:28 +0700

    A

--- rex/controllers/admin_controller.py
+++ rex/controllers/admin_controller.py
@@ -602,7 +602,7 @@ def auto_txid_withdraw():
     for x in data:
         respon_tx = ApiCoinpayment.get_withdrawal_info(id = x['id_coinpayment']) 
         print respon_tx
-        if respon_tx['error'] == 'ok' and respon_tx['result']['send_txid']:
+        if respon_tx['error'] == 'ok' and respon_tx['result']['send_txid'] is not None:
             db.wallets.update({'_id' : ObjectId(x['_id'])},{'$set' : {'txt_id' : respon_tx['result']['send_txid']}})
 
 
